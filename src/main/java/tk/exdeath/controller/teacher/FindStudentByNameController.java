13ff994ae9d0c58c7ced6a313e59d5c3a7a467ac
Death <yuman3539@gmail.com>
2020-11-01 12:18:47 +0300

    FindStudentByName was added  | v 0.57

--- src/main/java/tk/exdeath/controller/teacher/FindStudentByNameController.java
+++ src/main/java/tk/exdeath/controller/teacher/FindStudentByNameController.java
@@ -5,14 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import tk.exdeath.model.Student;
 import tk.exdeath.model.service.StudentService;
 
 @Controller
 public class FindStudentByNameController {
 
+    final String PATH = "teacher/findStudentByName";
+
     @GetMapping("/findStudentByName")
     public String findByName() {
-        return "findStudentByName";
+        return PATH;
     }
 
     @PostMapping("/findStudentByName")
@@ -21,7 +24,17 @@ public class FindStudentByNameController {
             @RequestParam String secondName, Model model) {
 
         StudentService studentService = new StudentService();
-        int studentID = studentService.readByName(firstName, secondName).getStudentID();
-        return "findStudentByName";
+        Student student = studentService.readByName(firstName, secondName);
+
+        if (studentExist(student)) {
+            return "redirect:/tasksByID?id=" + student.getStudentID();
+        }
+
+        model.addAttribute("Error", "Ученика с такими именем и фамилией не существует");
+        return PATH;
+    }
+
+    private boolean studentExist(Student student) {
+        return !student.getLogin().equals("null");
     }
 }
