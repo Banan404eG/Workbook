db4ed6b51a509aae904ba291ce2fbda120fa6af2
Death <yuman3539@gmail.com>
2020-11-05 18:55:52 +0300

    Little refactoring  | v 0.68

--- src/main/java/tk/exdeath/controller/workbook/task/TaskByIDController.java
+++ src/main/java/tk/exdeath/controller/workbook/task/TaskByIDController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import tk.exdeath.controller.teacher.LoggedTeacher;
 
 @Controller
 public class TaskByIDController {
@@ -14,8 +15,12 @@ public class TaskByIDController {
 
         TaskLibrary.TaskInfo taskInfo = TaskLibrary.getTaskInfo(id);
 
+        if (LoggedTeacher.getTeacher() != null) {
+            model.addAttribute("role", "teacher");
+        } else {
+            model.addAttribute("role", "watcher");
+        }
         model.addAttribute("studentAnswers", taskInfo.studentAnswers);
-        model.addAttribute("role", "teacher");
         model.addAttribute("rightAnswers", taskInfo.rightAnswers);
         return "workbook/" + taskInfo.lesson + "/" + taskInfo.grade + "/" + taskInfo.page;
     }
