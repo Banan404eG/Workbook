c31cd1fb278c685fe4f5df5cd6dfa6a7038a2001
Death <yuman3539@gmail.com>
2020-11-10 12:43:37 +0300

    Right answers were updated for new structure  | v 0.71

--- src/main/java/tk/exdeath/controller/workbook/task/AnswersByIDController.java
+++ src/main/java/tk/exdeath/controller/workbook/task/AnswersByIDController.java
@@ -11,9 +11,20 @@ import tk.exdeath.model.service.StudentService;
 @Controller
 public class AnswersByIDController {
 
+    final int RIGHT_ANSWERS_STUDENT_ID = 4;
+
+    Model model;
+    StudentService studentService;
+    String lesson;
+    int grade, page;
+
     @GetMapping("/taskByID")
     public String taskByID(@RequestParam int studentID,
-            @RequestParam int id, Model model) {
+                           @RequestParam int id, Model model) {
+
+
+        studentService = new StudentService();
+        this.model = model;
 
         Task task = getTask(studentID, id);
         if (task == null) {
@@ -21,23 +32,44 @@ public class AnswersByIDController {
             return "errorPage";
         }
 
-        if (LoggedTeacher.getTeacher() != null) {
-            model.addAttribute("role", "teacher");
-        } else {
-            model.addAttribute("role", "watcher");
-        }
+        lesson = task.getLesson();
+        grade = task.getGrade();
+        page = task.getPage();
         model.addAttribute("studentAnswers", task.getAnswers());
-        //model.addAttribute("rightAnswers", taskInfo.rightAnswers);
-        return "workbook/" + task.getLesson() + "/" + task.getGrade() + "/" + task.getPage();
+
+        setRightAnswers();
+        setRole();
+        return "workbook/" + lesson + "/" + grade + "/" + page;
     }
 
+
     private Task getTask(int studentID, int id) {
-        StudentService studentService = new StudentService();
+
         for (Task task : studentService.readByID(studentID).getTasks()) {
             if (task.getId() == id) {
                 return task;
             }
         }
         return null;
+    }
+
+    private void setRightAnswers() {
+
+        for (Task task : studentService.readByID(RIGHT_ANSWERS_STUDENT_ID).getTasks()) {
+            if (task.getLesson().equals(lesson) && task.getGrade() == grade && task.getPage() == page) {
+                model.addAttribute("rightAnswers", task.getAnswers());
+                return;
+            }
+        }
+        model.addAttribute("rightAnswers", "Правильные ответы не обнаружены");
+    }
+
+    private void setRole() {
+
+        if (LoggedTeacher.getTeacher() != null) {
+            model.addAttribute("role", "teacher");
+        } else {
+            model.addAttribute("role", "watcher");
+        }
     }
 }
